581c85e15b80dbf0839cb2853e690b2d0e6836c4
addirossi <addirossi@gmail.com>
2023-03-09 22:54:11 +0600

    Add sample view

--- logistic/views.py
+++ logistic/views.py
@@ -1,5 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.decorators import api_view
 from rest_framework.filters import SearchFilter
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Stock, Product
@@ -18,4 +20,8 @@ class StockViewSet(ModelViewSet):
     serializer_class = StockSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_fields = ['products']
-    search_fields = ['products__title', 'products__description']
+
+
+@api_view(['GET'])
+def sample_view(request):
+    return Response('This is checking!')
--- stocks_products/urls.py
+++ stocks_products/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from logistic.views import sample_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('logistic.urls')),
+    path('api/v1/test/', sample_view)
 ]
